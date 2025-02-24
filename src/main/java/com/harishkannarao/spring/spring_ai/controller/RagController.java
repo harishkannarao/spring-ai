26c58199674fc2b7cff8485800f02afe41e9428c
harishkannarao <harishkannarao@users.noreply.github.com>
2025-02-24 11:54:48 +0000

    Storing documents in vector store

--- src/main/java/com/harishkannarao/spring/spring_ai/controller/RagController.java
+++ src/main/java/com/harishkannarao/spring/spring_ai/controller/RagController.java
@@ -1,8 +1,11 @@
 package com.harishkannarao.spring.spring_ai.controller;
 
 import com.harishkannarao.spring.spring_ai.entity.InputDocument;
+import com.harishkannarao.spring.spring_ai.entity.InputMetaData;
 import org.springframework.ai.chat.model.ChatModel;
+import org.springframework.ai.document.Document;
 import org.springframework.ai.transformer.splitter.TokenTextSplitter;
+import org.springframework.ai.vectorstore.SearchRequest;
 import org.springframework.ai.vectorstore.VectorStore;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
@@ -11,6 +14,8 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
 
 @RestController
 public class RagController {
@@ -31,7 +36,15 @@ public class RagController {
 	@PostMapping("/ingest-document")
 	public ResponseEntity<Void> ingestDocument(
 		@RequestBody List<InputDocument> input) {
-
+		List<Document> vectorDocuments = input.stream()
+			.map(inputDocument -> {
+				Map<String, Object> metaData = inputDocument.metaData().stream()
+					.collect(Collectors.toUnmodifiableMap(InputMetaData::key, InputMetaData::value));
+				return new Document(inputDocument.content(), metaData);
+			})
+			.toList();
+		List<Document> splitDocuments = tokenTextSplitter.apply(vectorDocuments);
+		vectorStore.add(splitDocuments);
 		return ResponseEntity.accepted().build();
 	}
 }
