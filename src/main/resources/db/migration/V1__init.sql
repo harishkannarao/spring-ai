847dfa840c2b30c039f2dc44c1e14373de0cc69e
harishkannarao <harishkannarao@users.noreply.github.com>
2025-02-25 14:59:56 +0000

    Parameterising dimension and vector store index

--- src/main/resources/db/migration/V1__init.sql
+++ src/main/resources/db/migration/V1__init.sql
@@ -6,5 +6,7 @@ CREATE TABLE IF NOT EXISTS vector_store (
 	id uuid DEFAULT uuid_generate_v4() PRIMARY KEY,
 	content text,
 	metadata json,
-	embedding vector
+	embedding vector(${embeddingDimension})
 );
+
+CREATE INDEX ON vector_store USING ${vectorIndexType} (embedding vector_cosine_ops);
