b62a89aeeaf8116944e6136a2c039add6d508874
harishkannarao <harishkannarao@users.noreply.github.com>
2025-03-11 22:35:54 +0000

    Minor enhancement

--- src/main/java/com/harishkannarao/spring/spring_ai/controller/ImageController.java
+++ src/main/java/com/harishkannarao/spring/spring_ai/controller/ImageController.java
@@ -6,15 +6,12 @@ import org.springframework.ai.chat.prompt.Prompt;
 import org.springframework.ai.model.Media;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
-import org.springframework.core.io.ByteArrayResource;
 import org.springframework.http.ResponseEntity;
 import org.springframework.util.MimeType;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
-
-import java.io.IOException;
 
 import static java.util.Objects.requireNonNull;
 
@@ -31,9 +28,9 @@ public class ImageController {
 	@PostMapping(path = "image")
 	public ResponseEntity<String> imageChat(
 		@RequestParam("q") String input,
-		@RequestParam("file") MultipartFile file) throws IOException {
+		@RequestParam("file") MultipartFile file) {
 		UserMessage userMessage = new UserMessage(input,
-			new Media(MimeType.valueOf(requireNonNull(file.getContentType())), new ByteArrayResource(file.getBytes())));
+			new Media(MimeType.valueOf(requireNonNull(file.getContentType())), file.getResource()));
 		String result = imageChatClient.prompt(new Prompt(userMessage))
 			.call()
 			.content();
