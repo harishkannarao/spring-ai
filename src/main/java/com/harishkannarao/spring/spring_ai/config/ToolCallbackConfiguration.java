c4d82d9b76a9de2b3b81243452226329511c1624
harishkannarao <harishkannarao@users.noreply.github.com>
2025-03-22 20:20:12 +0000

    Weather Service

--- src/main/java/com/harishkannarao/spring/spring_ai/config/ToolCallbackConfiguration.java
+++ src/main/java/com/harishkannarao/spring/spring_ai/config/ToolCallbackConfiguration.java
@@ -2,6 +2,7 @@ package com.harishkannarao.spring.spring_ai.config;
 
 import com.harishkannarao.spring.spring_ai.tools.TicketBookingService;
 import com.harishkannarao.spring.spring_ai.tools.TicketInventoryService;
+import com.harishkannarao.spring.spring_ai.tools.WeatherService;
 import org.springframework.ai.tool.ToolCallback;
 import org.springframework.ai.tool.function.FunctionToolCallback;
 import org.springframework.context.annotation.Bean;
@@ -28,6 +29,18 @@ public class ToolCallbackConfiguration {
 				Input parameters are movie name and total number of tickets to book
 				""")
 			.inputType(TicketBookingService.Request.class)
+			.build();
+	}
+
+	@Bean
+	public ToolCallback createWeatherService(WeatherService weatherService) {
+		return FunctionToolCallback
+			.builder("weatherService", weatherService)
+			.description("""
+				Get the weather for the given location or village or town or city.
+				The request takes location along with unit in Celsius or Fahrenheit.
+				""")
+			.inputType(WeatherService.Request.class)
 			.build();
 	}
 }
