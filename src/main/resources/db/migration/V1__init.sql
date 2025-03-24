441ceb639a2dcb10471c968b85b1da15ff1a2761
harishkannarao <harishkannarao@users.noreply.github.com>
2025-03-24 17:33:11 +0000

    Added created time column

--- src/main/resources/db/migration/V1__init.sql
+++ src/main/resources/db/migration/V1__init.sql
@@ -4,6 +4,7 @@ CREATE EXTENSION IF NOT EXISTS "uuid-ossp";
 
 CREATE TABLE IF NOT EXISTS vector_store (
 	id uuid DEFAULT uuid_generate_v4() PRIMARY KEY,
+    created_time timestamp DEFAULT (timezone('utc', now())) NOT NULL,
 	content text,
 	metadata json,
 	embedding vector(${embeddingDimensions})
