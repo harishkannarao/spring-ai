dbc9514b448ef1a43eee1f1b37e31fa7404ca85b
harishkannarao <harishkannarao@users.noreply.github.com>
2025-06-18 17:00:27 +0100

    Chat with context

--- src/test/java/com/harishkannarao/spring/spring_ai/integration/ChatControllerIT.java
+++ src/test/java/com/harishkannarao/spring/spring_ai/integration/ChatControllerIT.java
@@ -1,12 +1,22 @@
 package com.harishkannarao.spring.spring_ai.integration;
 
+import com.harishkannarao.spring.spring_ai.entity.QuestionWithContext;
+import com.harishkannarao.spring.spring_ai.util.JsonUtil;
 import io.restassured.http.ContentType;
 import io.restassured.response.Response;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
 public class ChatControllerIT extends AbstractBaseIT {
+
+	private final JsonUtil jsonUtil;
+
+	@Autowired
+	public ChatControllerIT(JsonUtil jsonUtil) {
+		this.jsonUtil = jsonUtil;
+	}
 
 	@Test
 	public void simple_chat() {
@@ -34,5 +44,23 @@ public class ChatControllerIT extends AbstractBaseIT {
 
 		assertThat(response.statusCode()).isEqualTo(200);
 		assertThat(response.body().asString()).contains("1945");
+	}
+
+	@Test
+	public void chat_with_context() {
+		QuestionWithContext input = new QuestionWithContext(
+			"My name is Harish",
+			"What is my name?"
+		);
+
+		Response response = restClient()
+			.accept(ContentType.TEXT)
+			.contentType(ContentType.JSON)
+			.body(jsonUtil.toJson(input))
+			.post("/chat-with-context")
+			.andReturn();
+
+		assertThat(response.statusCode()).isEqualTo(200);
+		assertThat(response.body().asString()).contains("Harish");
 	}
 }
