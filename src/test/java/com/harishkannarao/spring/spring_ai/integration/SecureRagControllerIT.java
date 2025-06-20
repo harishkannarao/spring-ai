9531666ad444e6f0cbc8e1e6d8ac6d64cd5079dd
harishkannarao <harishkannarao@users.noreply.github.com>
2025-06-20 14:52:17 +0100

    Verifying secure rag for user1

--- src/test/java/com/harishkannarao/spring/spring_ai/integration/SecureRagControllerIT.java
+++ src/test/java/com/harishkannarao/spring/spring_ai/integration/SecureRagControllerIT.java
@@ -1,13 +1,34 @@
 package com.harishkannarao.spring.spring_ai.integration;
 
+import com.harishkannarao.spring.spring_ai.repository.SecureRagVectorRepository;
 import io.restassured.http.ContentType;
 import io.restassured.response.Response;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.io.ClassPathResource;
 import org.springframework.http.HttpHeaders;
+
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
 public class SecureRagControllerIT extends AbstractBaseIT {
+
+	private final SecureRagVectorRepository secureRagVectorRepository;
+	private final ClassPathResource secureRagDocument = new ClassPathResource(
+		"/secure_rag/secure_rag_document.json");
+
+	@Autowired
+	public SecureRagControllerIT(SecureRagVectorRepository secureRagVectorRepository) {
+		this.secureRagVectorRepository = secureRagVectorRepository;
+	}
+
+	@BeforeEach
+	public void setUp() {
+		secureRagVectorRepository.deleteAll();
+	}
 
 	@Test
 	public void returns_401_without_authentication() {
@@ -35,5 +56,43 @@ public class SecureRagControllerIT extends AbstractBaseIT {
 			.andReturn();
 
 		assertThat(aiResponse.statusCode()).isEqualTo(403);
+	}
+
+	@Test
+	public void verify_user1_response_with_rag() throws IOException {
+		String token = "user-token-1";
+		Response aiResponseBeforeRag = restClient()
+			.accept(ContentType.TEXT)
+			.headers(HttpHeaders.AUTHORIZATION, "Bearer " + token)
+			.queryParam("q", """
+				What is the best place to live in the UK
+				""")
+			.get("/secure-rag-chat")
+			.andReturn();
+
+		assertThat(aiResponseBeforeRag.statusCode()).isEqualTo(200);
+		assertThat(aiResponseBeforeRag.body().asString())
+			.doesNotContainIgnoringCase("Cricket");
+
+		Response docIngestionResponse = restClient()
+			.contentType(ContentType.JSON)
+			.body(secureRagDocument.getContentAsString(StandardCharsets.UTF_8))
+			.post("/ingest-secure-document")
+			.andReturn();
+
+		assertThat(docIngestionResponse.getStatusCode()).isEqualTo(204);
+
+		Response aiResponseAfterRag = restClient()
+			.accept(ContentType.TEXT)
+			.headers(HttpHeaders.AUTHORIZATION, "Bearer " + token)
+			.queryParam("q", """
+				What is the best place to live in the UK
+				""")
+			.get("/secure-rag-chat")
+			.andReturn();
+
+		assertThat(aiResponseAfterRag.statusCode()).isEqualTo(200);
+		assertThat(aiResponseAfterRag.body().asString())
+			.containsIgnoringCase("Cricket");
 	}
 }
